63a8f5fe51b992a29527a9dbce1092c0c69ab235
CarlosMontesdeoca <mateo.game@hotmail.com>
2023-05-19 13:54:06 -0500

    conexion a la base de datos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require('express')
-
 const app = express()
 
+const  mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/cotizador')
+    .then(db => console.log('Db is connected'))
+    .catch(error => console.error(error));
+
+app.get('/test', (req, res) => res.json({
+    message: "conection succes",
+    status: 200,
+}))
 app.listen(8080, () => console.log('Server is listening'))
