bde1f331177bed7f72bcbb22304670f8933582c8
CarlosMontesdeoca <mateo.game@hotmail.com>
2023-05-25 16:34:36 -0500

    esquema de datos

--- models/model.js
+++ models/model.js
@@ -4,15 +4,17 @@ const QuoterSchema = new mongoose.Schema({
     N_offert: { type: String }, 
     advisor: { type: String }, 
     ruc: { type: String }, 
-    client: { type: String }, 
+    client: { type: String },
+    id_plant: {type: Number},
     plant: { type: String }, 
     address: { type: String }, 
     contact: { type: String },
     email: { type: String }, 
     phone: { type: String },
+    bals: { type: Array },
     services: { type: Array },
     products: { type: Array },
-    state: {type: String}
+    state: {type: String, default: 'A'}
 }, {
     timestamps: true,
     versionKey: false
