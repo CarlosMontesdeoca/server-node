f4d5612f03371cd56a293b06b395a5cc7f9874f6
CarlosMontesdeoca <mateo.game@hotmail.com>
2023-09-28 13:24:46 -0500

    tamaño de imagenes

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ database.on('error', (error) => {
 database.once('connected', () => {
     console.log('Database Connected');
 })
+
 const app = express();
 app.use(cors())
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 const routes = require('./routes/routes');
 
